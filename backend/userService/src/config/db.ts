f2fe0cdd0600c223284216109f45b4a7ffdd2ce7
Sibuchanda <sibuchanda457@gmail.com>
2025-07-07 01:40:35 +0530

    Connected Redis, MongoDB, RbitMQ

---
+++ backend/userService/src/config/db.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connectDB = async ()=>{
+    const url = process.env.MONGO_URI;
+    if(!url){
+        throw new Error("Mongo URI is not defined in environment variable");
+    }
+
+    try{
+      await mongoose.connect(url,{
+        dbName: "MicroserviceChatApp"
+      });
+      console.log("MongoDB Connected successfully...");
+    }catch(err){
+        console.log("MongoDB connection failed!");
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ backend/userService/src/config/rabbitmq.ts
@@ -0,0 +1,22 @@
+import ampq from 'amqplib';
+
+let channel: ampq.Channel
+
+const connectRabbitMQ = async()=>{
+    try{
+     const connection = await ampq.connect({
+        protocol: "amqp",
+        hostname: process.env.RabbitMQ_Host,
+        port: 5672,
+        username: process.env.RabbitMQ_Username,
+        password: process.env.RabbitMQ_Password,
+     });
+
+     channel = await connection.createChannel();
+     console.log("RabbitMQ connected successfully...");
+    }catch(err){
+        console.log("Error occured during RabbitMQ connection : ", err);
+    }
+}
+
+export default connectRabbitMQ;
