8fe31aea9d09be3e0e6b6eda83a55bbe1df15ff0
Sibuchanda <sibuchanda457@gmail.com>
2025-08-05 20:38:44 +0530

    SideBar for main chat page is implemented

--- frontend/src/chat/ChatApp.tsx
+++ frontend/src/chat/ChatApp.tsx
@@ -1,25 +1,71 @@
-import { useEffect } from "react";
-import { useAppData } from "../context/AppContext";
+import { useEffect, useState } from "react";
+import { useAppData, type User } from "../context/AppContext";
 import { useNavigate } from "react-router-dom";
 import Loading from "../verify/Loading";
+import ChatSidebar from "../component/ChatSidebar";
 
-
+export interface Message {
+  _id: string;
+  chatId: string;
+  sender: string;
+  text?: string;
+  image?: {
+    url: string;
+    publicId: string;
+  };
+  messageType: "text" | "image";
+  seen: boolean;
+  seenAt?: string;
+  createdAt: string;
+}
 
 const ChatApp = () => {
-  const { loading, isAuth } = useAppData();
-
+  const {
+    loading,
+    isAuth,
+    logoutUser,
+    chats,
+    user: loggedInUser,
+    users,
+    fetchChats,
+    setChats,
+  } = useAppData();
   const navigateTo = useNavigate();
+
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
+  const [message, setMessage] = useState("");
+  const [siderbarOpen, setSiderbarOpen] = useState(false);
+  const [messages, setMessages] = useState<Message[] | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [showAllUser, setShowAllUser] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
+  const [typingTimeOut, setTypingTimeOut] = useState<ReturnType<
+    typeof setTimeout
+  > | null>(null);
 
   useEffect(() => {
     if (!isAuth && !loading) {
       navigateTo("/login");
     }
   }, [isAuth, navigateTo, loading]);
-  if(loading) return <Loading/>;  
 
+  const handleLogout = () => logoutUser();
+
+  if (loading) return <Loading />;
   return (
     <>
-      <p> This is ChatApp ...</p>
+      <ChatSidebar
+        sidebarOpen={siderbarOpen}
+        setSidebarOpen={setSiderbarOpen}
+        showAllUsers={showAllUser}
+        setShowAllUsers={setShowAllUser}
+        users={users}
+        loggedInUser={loggedInUser}
+        chats={chats}
+        selectedUser={selectedUser}
+        setSelectedUser={setSelectedUser}
+        handleLogout={handleLogout}
+      />
     </>
   );
 };
